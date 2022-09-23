136b566214c33c1f5da1993dd2b665de4da3df98
Liuba Dmytro <lyubadmitry2@gmail.com>
2022-09-23 14:13:42 +0300

    Добавлена корзина

--- js/script.js
+++ js/script.js
@@ -73,3 +73,28 @@ $('.owl-carousel.reviews').owlCarousel({
   items: 2,
   navText: ["", "<span>Ещё</span><img src='../img/reviews-arrow.svg'>"]
 })
+
+
+jQuery(function(){
+  var j = jQuery; //Just a variable for using jQuery without conflicts
+  var addInput1 = '.qty1'; //This is the id of the input you are changing
+  var n = 1; //n is equal to 1
+  
+  //Set default value to n (n = 1)
+  j(addInput1).val(n);
+
+  //On click add 1 to n
+  j('.plus').on('click', function(){
+    // j(addInput1).val(++n);
+    j(this).parent(".range").children(addInput1).val(++n);
+  })
+
+  j('.min').on('click', function(){
+    //If n is bigger or equal to 1 subtract 1 from n
+    if (n > 1) {
+      j(this).parent(".range").children(addInput1).val(--n);
+    } else {
+      //Otherwise do nothing
+    }
+  });
+});
